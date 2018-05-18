354886d5355d31bd4f97563667c966db930901a4
joshuamkite <joshuamkite@gmail.com>
2018-05-18 11:37:18 +0100

    moved dns name output to outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "user_data_ami_linux" {
   description = "cloud-config user data used to initialize ami linux instances which are run as ecs/k8s nodes"
   value       = "${module.bastion_user_data.user_data_ami_linux}"
 }
+
+output "service_dns_entry" {
+  description = "dns-registered url for service and host"
+  value       = "${var.environment_name}-${data.aws_region.current.name}-bastion-service.${var.dns_domain}"
+}

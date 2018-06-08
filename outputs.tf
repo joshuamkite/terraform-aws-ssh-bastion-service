07970ea7b75e638b672001d11143780e14e00ef6
Joshua Kite <joshuamkite@gmail.com>
2018-06-08 16:37:07 +0100

    move user_data creation back into main plan

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,3 @@
-output "user_data_bastion" {
-  description = "cloud-config user data used to initialize bastion at start up"
-  value       = "${module.bastion_user_data.user_data_bastion}"
-}
-
-output "user_data_ami_linux" {
-  description = "cloud-config user data used to initialize ami linux instances which are run as ecs/k8s nodes"
-  value       = "${module.bastion_user_data.user_data_ami_linux}"
-}
-
 output "service_dns_entry" {
   description = "dns-registered url for service and host"
   value       = "${var.environment_name}-${data.aws_region.current.name}-${data.aws_vpc.main.id}-bastion-service.${var.dns_domain}"

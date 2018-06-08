1eec5232d73a120d182bcabfd9661cc9f2bcc454
Joshua Kite <joshuamkite@gmail.com>
2018-06-08 16:36:16 +0100

    update iam service role with conditional creation

--- iam_service_role/main.tf
+++ iam_service_role/main.tf
@@ -1,6 +1,11 @@
 #get aws region for use later in plan
 data "aws_region" "current" {}
 
+locals {
+  assume_role_yes = "${var.assume_role_arn != "" ? 1 : 0}"
+  assume_role_no  = "${var.assume_role_arn == "" ? 1 : 0}"
+}
+
 ###########
 #aws iam role for host
 ##############
@@ -37,7 +42,7 @@ resource "aws_iam_policy" "check_ssh_authorized_keys" {
   name = "${var.bastion_name}_check_ssh_authorized_keys"
 
   description = "Allow querying aws to obtain list of users with their ssh public keys"
-  count       = "${var.create_iam_service_role}"
+  count       = "${local.assume_role_yes}"
 
   policy = <<EOF
 {
@@ -66,10 +71,15 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "check_ssh_authorized_keys" {
-  role  = "${aws_iam_role.bastion_service_role.name}"
-  count = "${var.create_iam_service_role}"
-
+  role       = "${aws_iam_role.bastion_service_role.name}"
+  count      = "${local.assume_role_yes}"
   policy_arn = "${aws_iam_policy.check_ssh_authorized_keys.arn}"
+}
+
+resource "aws_iam_role_policy_attachment" "assume_role_in_master_account" {
+  role       = "${aws_iam_role.bastion_service_role.name}"
+  count      = "${local.assume_role_yes}"
+  policy_arn = "${var.assume_role_arn}"
 }
 
 output "bastion_service_role_name" {
--- iam_service_role/variables.tf
+++ iam_service_role/variables.tf
@@ -2,7 +2,7 @@ variable "bastion_name" {
   description = "the name that will prefix bastion roles/policies"
 }
 
-variable "create_iam_service_role" {
-  description = "create bastion service role and policies (standalone account) boolean"
-  default     = "1"
+
+variable "assume_role_arn" {
+  description = "arn for role to assume in separate identity account if used"
 }

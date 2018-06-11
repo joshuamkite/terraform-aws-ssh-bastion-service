71fefac2d9094e2c0d4347d97b9b0b720d72104f
Joshua Kite <joshuamkite@gmail.com>
2018-06-11 01:07:58 +0100

    working with interpolated policy arn

--- iam_child.tf
+++ iam_child.tf
@@ -21,41 +21,12 @@ resource "aws_iam_role" "bastion_service_assume_role" {
 EOF
 }
 
-# resource "aws_iam_role" "bastion_service_assume_role" {
-#   count              = "${local.assume_role_yes}"
-#   name               = "${var.environment_name}-${data.aws_region.current.name}-${var.vpc}_bastion"
-#   assume_role_policy = "${data.aws_iam_policy_document.bastion_service_assume_role_policy.json}"
-# }
-
 #Instance profile
 resource "aws_iam_instance_profile" "bastion_service_assume_role_profile" {
   name  = "${var.environment_name}-${data.aws_region.current.name}-${var.vpc}_bastion"
   count = "${local.assume_role_yes}"
   role  = "${aws_iam_role.bastion_service_assume_role.name}"
 }
-
-#policy to parent account
-# resource "aws_iam_policy" "bastion_service_assume_role_in_parent" {
-#   name = "${var.environment_name}-${data.aws_region.current.name}-${var.vpc}_lookup"
-
-#   description = "Allow querying parent AWS account to obtain list of users with their ssh public keys"
-#   count       = "${local.assume_role_yes}"
-
-#   policy = <<EOF
-# {
-#     "Version": "2012-10-17",
-#     "Statement": [
-#         {
-#             "Effect": "Allow",
-#             "Action": "sts:AssumeRole",
-#             "Resource": [
-#                 "arn:aws:iam::144992683770:role/bastion-assume-role-poc"
-#             ]
-#         }
-#     ]
-# }
-# EOF
-# }
 
 data "aws_iam_policy_document" "bastion_service_assume_role_in_parent" {
   count = "${local.assume_role_yes}"
@@ -67,14 +38,9 @@ data "aws_iam_policy_document" "bastion_service_assume_role_in_parent" {
       "sts:AssumeRole",
     ]
 
-    # principals {
-    #   type = "AWS"
-
     resources = [
-      "arn:aws:iam::144992683770:role/bastion-assume-role-poc",
+      "${var.assume_role_arn}",
     ]
-
-    # }
   }
 }
 

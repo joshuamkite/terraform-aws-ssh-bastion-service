3c315ac7cd52d7a7d560d9d35f746897bb32e668
Joshua Kite <joshuamkite@gmail.com>
2018-06-11 01:04:10 +0100

    works assume role with interpolatable policy

--- iam_child.tf
+++ iam_child.tf
@@ -35,26 +35,53 @@ resource "aws_iam_instance_profile" "bastion_service_assume_role_profile" {
 }
 
 #policy to parent account
-resource "aws_iam_policy" "bastion_service_assume_role_in_parent" {
-  name = "${var.environment_name}-${data.aws_region.current.name}-${var.vpc}_lookup"
+# resource "aws_iam_policy" "bastion_service_assume_role_in_parent" {
+#   name = "${var.environment_name}-${data.aws_region.current.name}-${var.vpc}_lookup"
 
-  description = "Allow querying parent AWS account to obtain list of users with their ssh public keys"
-  count       = "${local.assume_role_yes}"
+#   description = "Allow querying parent AWS account to obtain list of users with their ssh public keys"
+#   count       = "${local.assume_role_yes}"
 
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "sts:AssumeRole",
-            "Resource": [
-                "arn:aws:iam::144992683770:role/bastion-assume-role-poc"
-            ]
-        }
+#   policy = <<EOF
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Effect": "Allow",
+#             "Action": "sts:AssumeRole",
+#             "Resource": [
+#                 "arn:aws:iam::144992683770:role/bastion-assume-role-poc"
+#             ]
+#         }
+#     ]
+# }
+# EOF
+# }
+
+data "aws_iam_policy_document" "bastion_service_assume_role_in_parent" {
+  count = "${local.assume_role_yes}"
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sts:AssumeRole",
     ]
+
+    # principals {
+    #   type = "AWS"
+
+    resources = [
+      "arn:aws:iam::144992683770:role/bastion-assume-role-poc",
+    ]
+
+    # }
+  }
 }
-EOF
+
+resource "aws_iam_policy" "bastion_service_assume_role_in_parent" {
+  count  = "${local.assume_role_yes}"
+  name   = "${var.environment_name}-${data.aws_region.current.name}-${var.vpc}_bastion"
+  policy = "${data.aws_iam_policy_document.bastion_service_assume_role_in_parent.json}"
 }
 
 resource "aws_iam_role_policy_attachment" "bastion_service_assume_role" {

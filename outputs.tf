2dc990c68c157520ac1777d427c41c0c819650ab
Joshua Kite <joshuamkite@gmail.com>
2018-07-11 11:01:50 +0100

    make debug outputs parsable as module

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "service_dns_entry" {
   value       = "${var.environment_name}-${data.aws_region.current.name}-${data.aws_vpc.main.id}-bastion-service.${var.dns_domain}"
 }
 
-output "policy_example_for_parent_account_(empty_if_not_used)" {
+output "policy_example_for_parent_account_empty_if_not_used" {
   description = "You must apply an IAM policy with trust realtionship identical or compatible with this in your other AWS account for IAM lookups to function there with STS:AssumeRole and allow users to login"
   value       = "${join("", data.template_file.sample_policies_for_parent_account.*.rendered)}"
 }

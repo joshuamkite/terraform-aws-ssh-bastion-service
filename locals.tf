32d8c45290f34f28af37605554250ddc2f877bef
Joshua Kite <joshuamkite@gmail.com>
2018-09-02 21:57:17 +0100

    initial working version to interpolate default for docker build stanza

--- locals.tf
+++ locals.tf
@@ -21,3 +21,15 @@ locals {
   assume_role_yes = "${var.assume_role_arn != "" ? 1 : 0}"
   assume_role_no  = "${var.assume_role_arn == "" ? 1 : 0}"
 }
+
+##########################
+# Logic tests for  building docker container locally or using custom container
+##########################
+# locals {
+#   custom_container_yes = "${var.custom_container != "" ? 1 : 0}"
+#   custom_container_no  = "${var.custom_container == "" ? 1 : 0}"
+# }
+
+locals {
+  container_build = "${var.custom_container == "" ? "cd /opt/sshd_worker\n        docker build -t sshd_worker ." : var.custom_container}"
+}

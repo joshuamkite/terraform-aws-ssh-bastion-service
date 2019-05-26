8ab2ff12e305d09aa6235724d28172f0223c376c
joshuamkite <joshuamkite@gmail.com>
2019-05-26 11:52:48 +0100

    WIP broken version 12 example

--- examples/full-with-public-ip/outputs.tf
+++ examples/full-with-public-ip/outputs.tf
@@ -1,23 +1,24 @@
 output "bastion_sg_id" {
   description = "Security Group id of the bastion host"
-  value       = "${module.ssh-bastion-service.bastion_sg_id}"
+  value       = module.ssh-bastion-service.bastion_sg_id
 }
 
 output "lb_dns_name" {
   description = "aws load balancer dns"
-  value       = "${module.ssh-bastion-service.lb_dns_name}"
+  value       = module.ssh-bastion-service.lb_dns_name
 }
 
 output "lb_zone_id" {
-  value = "${module.ssh-bastion-service.lb_zone_id}"
+  value = module.ssh-bastion-service.lb_zone_id
 }
 
 output "bastion_service_role_name" {
   description = "role created for service host asg - if created without assume role"
-  value       = "${module.ssh-bastion-service.bastion_service_role_name}"
+  value       = module.ssh-bastion-service.bastion_service_role_name
 }
 
 output "lb_arn" {
   description = "aws load balancer arn"
-  value       = "${module.ssh-bastion-service.lb_arn}"
+  value       = module.ssh-bastion-service.lb_arn
 }
+

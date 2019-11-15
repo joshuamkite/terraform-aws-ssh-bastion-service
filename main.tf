7d626c361f99f9a44072a4a1b97b0b4f7c535e37
Raquel Carvalho <raquel.carvalho@dazn.com>
2019-11-15 10:02:39 +0100

    PLAT-1652: Enable spot instances on prod envirnoment

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ resource "aws_autoscaling_group" "bastion-service" {
   mixed_instances_policy {
     instances_distribution {
       on_demand_base_capacity                  = var.on_demand_base_capacity
-      on_demand_percentage_above_base_capacity = var.enviroment_name != "prod" ? 0 : 100
+      on_demand_percentage_above_base_capacity = 0
     }
 
     launch_template {

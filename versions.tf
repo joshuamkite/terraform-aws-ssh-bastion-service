40e88836f02367e4267d1b52184650d08ec598c2
joshuamkite <joshuamkite@gmail.com>
2020-08-22 11:04:16 +0100

    update providers for tf13

--- versions.tf
+++ versions.tf
@@ -1,4 +1,15 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
 }

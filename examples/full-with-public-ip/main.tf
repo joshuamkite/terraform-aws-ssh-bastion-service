4b91a5124376a669e045dc8b87281dbfebb7d26f
joshuamkite <joshuamkite@gmail.com>
2022-02-08 16:58:18 +0000

    restore examples/full.../main.tf

--- examples/full-with-public-ip/main.tf
+++ examples/full-with-public-ip/main.tf
@@ -47,8 +47,8 @@ variable "everyone-cidr" {
 }
 
 module "ssh-bastion-service" {
-  # source = "joshuamkite/ssh-bastion-service/aws"
-  source                        = "../../"
+  source = "joshuamkite/ssh-bastion-service/aws"
+  # source                        = "../../"
   aws_region                    = var.aws_region
   environment_name              = var.environment_name
   vpc                           = aws_vpc.bastion.id
